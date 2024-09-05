9ac66df03df14eccb0edea41ca65dccca7f61f56
AdhiVenkatesh <aadhi494@gmail.com>
2024-09-05 13:59:47 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Menu from "./components/Menu";
+import Settings from "./components/Settings";
+
+import Home from "./components/Home";
+import Live from "./components/Live";
+import Movies from "./components/Movies";
+import Sports from "./components/Sports";
+import TVShows from "./components/TVShows";
+import NotFound from "./components/NotFound";
+
+import LanguageContext from "./context/LanguageContext";
+
+import "./App.css";
+
+class App extends Component {
+  state = { activeLanguage: "en" };
+
+  changeLanguage = (activeLanguage) => {
+    this.setState({ activeLanguage });
+  };
+
+  render() {
+    const { activeLanguage } = this.state;
+    return (
+      <LanguageContext.Provider
+        value={{ activeLanguage, changeLanguage: this.changeLanguage }}
+      >
+        <>
+          <BrowserRouter>
+            <nav className="background-nav">
+              <Menu />
+              <Settings />
+            </nav>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/live" element={<Live />} />
+              <Route exact path="/movies" element={<Movies />} />
+              <Route exact path="/sports" element={<Sports />} />
+              <Route exact path="/tvshows" element={<TVShows />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      </LanguageContext.Provider>
+    );
+  }
 }
 
 export default App;
